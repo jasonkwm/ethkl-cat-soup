b94b3d5a0657d730128e3a0777bcaa38938bdc63
mseong <mseong123@hotmail.com>
2024-10-04 19:19:23 +0800

    fix: commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,47 @@
-"use client";
-import { useWeb3AuthContext } from "@/context/Web3AuthProvider";
-import LogoutButton from "@/components/LogoutButton";
+"use client"
+import { SelectionScreen } from '../Component/SelectionScreen.js';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import { useState, useEffect } from 'react';
+import web
+
+
+
 export default function Home() {
+  const [page, setPage] = useState("")
+  const [walletInstalled, setWalletInstalled] = useState(false)
+  const [account, setAccount] = useState("")
+  const [error, setError] = useState("")
+  const [provider, setProvider] = useState("")
   const { isLoggedIn, userInfo } = useWeb3AuthContext();
   console.log("userInfo");
+  
   if (!isLoggedIn && !userInfo) return;
+  
+  useEffect(()=>{
+    window.ethereum? setWalletInstalled(true):setWalletInstalled(false)
+  },[])
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>CAT SOUP</h1>
-      <p>start of greateness</p>
-      <LogoutButton />
+    <div class="container">
+      
+      <div className="d-flex flex-column justify-content-center vh-100 align-items-center">
+        {page === "selectionScreen"? <SelectionScreen/> : 
+          <Button onClick={async function(){
+            try {
+              await window.ethereum.request({ method: "eth_requestAccounts" });
+
+
+            }
+            catch(e) {
+              setError(e.message)
+            }
+
+          }} className="primary my-2" disabled={walletInstalled? false:true}>Connect</Button>}
+          {walletInstalled? null:<Alert className="warning">Wallet Extension Not Installed. Please install Metamask.</Alert>}
+          {walletInstalled? null:<Alert className="warning">Wallet Extension Not Installed. Please install Metamask.</Alert>}
+      </div>
     </div>
   );
 }
