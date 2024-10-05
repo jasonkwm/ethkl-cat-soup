af02afb0a404aed1c10c1fc3afdb34f8fd0fbc7c
mseong <mseong123@hotmail.com>
2024-10-05 21:24:33 +0800

    modified and finalised the graph

--- the-graph/CryptoSurvey-scroll-sepolia/src/crypto-survey-v-1.ts
+++ the-graph/CryptoSurvey-scroll-sepolia/src/crypto-survey-v-1.ts
@@ -1,3 +1,5 @@
+import { store  } from '@graphprotocol/graph-ts'
+
 import {
   ApprovalForAll as ApprovalForAllEvent,
   CreatePermissionEvent as CreatePermissionEventEvent,
@@ -14,18 +16,93 @@ import {
 } from "../generated/CryptoSurveyV1/CryptoSurveyV1"
 import {
   ApprovalForAll,
-  CreatePermissionEvent,
-  CreateRequestEvent,
-  CreateSurveyEvent,
-  CreateSurveyMetadataEvent,
+  Survey,
+  Request,
+  Permission,
+  SurveyReply,
   RoleAdminChanged,
   RoleGranted,
   RoleRevoked,
-  SubmitSurveyReplyEvent,
   TransferBatch,
   TransferSingle,
   URI
 } from "../generated/schema"
+
+export function handleCreateSurveyEvent(event: CreateSurveyEventEvent): void {
+  let entity = Survey.load(event.params.surveyId.toHexString())
+  if (entity == null)
+    entity = new Survey(
+      event.params.surveyId.toHexString()
+  )
+  entity.surveyId = event.params.surveyId
+  entity.name = event.params.name
+  entity.description = event.params.description
+  entity.imageCID = event.params.imageCID
+  entity.encryptedCID = event.params.encryptedCID
+  entity.owner = event.params.surveyAddress
+  entity.blockTimestamp = event.block.timestamp
+
+  entity.save()
+}
+
+export function handleCreateSurveyMetadataEvent(
+  event: CreateSurveyMetadataEventEvent
+): void {
+  let entity = Survey.load(event.params.surveyId.toHexString())
+  if (entity == null)
+    entity = new Survey(
+      event.params.surveyId.toHexString()
+  )
+  entity.surveyId = event.params.surveyId
+  entity.maxReply = event.params.maxReply
+  entity.incentive = event.params.incentive
+  entity.blockTimestamp = event.block.timestamp
+  entity.save()
+}
+
+export function handleCreateRequestEvent(event: CreateRequestEventEvent): void {
+  let entity = new Request(
+    event.params.surveyId.toHexString() + event.params.requestAddress.toHexString()
+  )
+  entity.survey = event.params.surveyId.toHexString()
+  entity.requestAddress = event.params.requestAddress
+  entity.description = event.params.description
+  entity.publicKey = event.params.publicKey
+  entity.blockTimestamp = event.block.timestamp
+  entity.save()
+}
+
+export function handleCreatePermissionEvent(
+  event: CreatePermissionEventEvent
+): void {
+  let entity = new Permission(
+    event.params.surveyId.toHexString() + event.params.requestAddress.toHexString()
+  )
+  store.remove('Request', event.params.surveyId.toHexString() + event.params.requestAddress.toHexString())
+  entity.survey = event.params.surveyId.toHexString()
+  entity.requestAddress = event.params.requestAddress
+  entity.encryptedCID = event.params.encryptedCID
+  entity.blockTimestamp = event.block.timestamp
+  entity.save()
+}
+
+export function handleSubmitSurveyReplyEvent(
+  event: SubmitSurveyReplyEventEvent
+): void {
+  let entity = new SurveyReply(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  store.remove('Permission', event.params.surveyId.toHexString() + event.params.replyAddress.toHexString())
+  entity.survey = event.params.surveyId.toHexString()
+  entity.incentive = event.params.incentive
+  entity.encryptedCID = event.params.encryptedCID
+  entity.replyAddress = event.params.replyAddress
+
+  
+  entity.blockTimestamp = event.block.timestamp
+
+  entity.save()
+}
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
@@ -42,74 +119,10 @@ export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   entity.save()
 }
 
-export function handleCreatePermissionEvent(
-  event: CreatePermissionEventEvent
-): void {
-  let entity = new CreatePermissionEvent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.surveyId = event.params.surveyId
-  entity.requestAddress = event.params.requestAddress
-  entity.encryptedCID = event.params.encryptedCID
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
-  entity.save()
-}
 
-export function handleCreateRequestEvent(event: CreateRequestEventEvent): void {
-  let entity = new CreateRequestEvent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.surveyId = event.params.surveyId
-  entity.requestAddress = event.params.requestAddress
-  entity.description = event.params.description
-  entity.publicKey = event.params.publicKey
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCreateSurveyEvent(event: CreateSurveyEventEvent): void {
-  let entity = new CreateSurveyEvent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.surveyId = event.params.surveyId
-  entity.name = event.params.name
-  entity.description = event.params.description
-  entity.imageCID = event.params.imageCID
-  entity.encryptedCID = event.params.encryptedCID
-  entity.surveyAddress = event.params.surveyAddress
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCreateSurveyMetadataEvent(
-  event: CreateSurveyMetadataEventEvent
-): void {
-  let entity = new CreateSurveyMetadataEvent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.surveyId = event.params.surveyId
-  entity.maxReply = event.params.maxReply
-  entity.incentive = event.params.incentive
-  entity.owner = event.params.owner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
   let entity = new RoleAdminChanged(
@@ -156,23 +169,7 @@ export function handleRoleRevoked(event: RoleRevokedEvent): void {
   entity.save()
 }
 
-export function handleSubmitSurveyReplyEvent(
-  event: SubmitSurveyReplyEventEvent
-): void {
-  let entity = new SubmitSurveyReplyEvent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.surveyId = event.params.surveyId
-  entity.incentive = event.params.incentive
-  entity.encryptedCID = event.params.encryptedCID
-  entity.replyAddress = event.params.replyAddress
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
   let entity = new TransferBatch(
@@ -198,7 +195,7 @@ export function handleTransferSingle(event: TransferSingleEvent): void {
   entity.operator = event.params.operator
   entity.from = event.params.from
   entity.to = event.params.to
-  entity.CryptoSurveyV1_id = event.params.id
+ 
   entity.value = event.params.value
 
   entity.blockNumber = event.block.number
@@ -211,7 +208,7 @@ export function handleTransferSingle(event: TransferSingleEvent): void {
 export function handleURI(event: URIEvent): void {
   let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.value = event.params.value
-  entity.CryptoSurveyV1_id = event.params.id
+  
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
